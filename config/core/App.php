6110d18e2d6cf7798c653abacc2c4a4a63b97286
bara <bayebara2000@gmail..com>
2025-07-28 18:49:53 +0000

    Ajout du Dockerfile pour le déploiement

--- config/core/App.php
+++ config/core/App.php
@@ -43,14 +43,70 @@ class App
     {
         self::initialize();
 
-        foreach (self::$container as $category => $services) {
-            if (array_key_exists($key, $services)) {
-                $factory = $services[$key];
-                return $factory();
+        if (isset(self::$instances[$key])) {
+            return self::$instances[$key];
+        }
+
+        if (!isset(self::$services[$key])) {
+            throw new Exception("Dependency not found: " . $key);
+        }
+
+        $className = self::$services[$key];
+        $object = self::build($className);
+        self::$instances[$key] = $object;
+        return $object;
+    }
+
+    private static function build(string $className): object
+    {
+        // Cas particulier pour Session (singleton)
+        if ($className === \App\Core\Session::class) {
+            return \App\Core\Session::getInstance();
+        }
+
+        // Exemple d'extraction des arguments dans App.php
+        if ($className === \PDO::class) {
+            $dependencies = Yaml::parseFile(__DIR__ . '/../../config/service.yaml');
+            $pdoConfig = $dependencies['core']['database'] ?? null;
+            if ($pdoConfig && isset($pdoConfig['arguments'])) {
+                $args = array_map(function($arg) {
+                    if (preg_match('/^%(.*)%$/', $arg, $matches)) {
+                        return $_ENV[$matches[1]];
+                    }
+                    return $arg;
+                }, $pdoConfig['arguments']);
+                return new \PDO(...$args);
+            }
+            throw new \Exception('PDO configuration missing in service.yaml');
+        }
+
+        if (!class_exists($className)) {
+            throw new Exception("Class '$className' not found.");
+        }
+
+        $reflection = new ReflectionClass($className);
+
+        if (!$reflection->getConstructor()) {
+            return new $className();
+        }
+
+        $dependencies = [];
+        foreach ($reflection->getConstructor()->getParameters() as $param) {
+            $type = $param->getType();
+            if ($type && !$type->isBuiltin()) {
+                $depClass = $type->getName();
+                // Recherche l'alias correspondant à la classe attendue
+                $depKey = array_search($depClass, self::$services, true);
+                if ($depKey === false) {
+                    throw new Exception("No service defined for class $depClass");
+                }
+                $dependencies[] = self::getDependency($depKey);
+            } else {
+                // Valeur par défaut ou null si non injectable
+                $dependencies[] = $param->isDefaultValueAvailable() ? $param->getDefaultValue() : null;
             }
         }
 
-        throw new \Exception("Dependency not found: " . $key);
+        return $reflection->newInstanceArgs($dependencies);
     }
 }
-
